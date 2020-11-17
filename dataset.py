621641dcf87a629a94859916963fb03595789dc7
Esteve Valls <43722285+Evm7@users.noreply.github.com>
2020-11-17 22:32:08 +0100

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -25,7 +25,7 @@ print ("MODULE PATH" + module_path)
 
 class DatasetConfig(Config):
     NAME = "llampuga"
-    IMAGES_PER_GPU = 2
+    IMAGES_PER_GPU = 1
     NUM_CLASSES = 1 + 2  #toni 1 +26
     STEPS_PER_EPOCH = 190 # 190 / 4
     VALIDATION_STEPS = 60
