199f320cd52f3ea1a84b88867155244795a182b3
Esteve Valls <43722285+Evm7@users.noreply.github.com>
2020-12-10 12:07:41 +0100

    Update demo_llampuga.py

--- demo_llampuga.py
+++ demo_llampuga.py
@@ -253,7 +253,7 @@ class Demo_LLampuga():
                          learning_rate=self.config.LEARNING_RATE,
                          epochs=epochs,
                          layers="all",
-                         augmentation=self.seq_of_aug
+                         #augmentation=self.seq_of_aug
                          )
 
     def evaluation(self, number_of_images, confussion, directory):
